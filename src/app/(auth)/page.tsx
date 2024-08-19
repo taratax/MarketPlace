7cdc625293983a617e218cdf930a16baec8ddec8
Gabriel Kieruzel <gabriel.kieruzel@protonmail.com>
2024-08-19 13:33:13 +0200

    implement first step to auth section

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div className='container relative flex pr-20 flex-col items-center justify-center lg:px-0'>
+                <div className='mx-auto flex w-full flex-col justofy-center space-y-6 sm:w-[350px]'>
+                    <div className='flex flex-col items-center space-y-2 text-center'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default page
